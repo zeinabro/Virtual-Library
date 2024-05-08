68eb55ce30d25783e48147e03421455789e1fd52
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 14:41:11 +0100

    music class

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,28 @@
 // import the Media class:
-
+const Media = require("./Media")
 // create your Music class:
+class Music extends Media {
+    constructor(title,year,genre,artist,length){
+        super(title,year,genre)
+        this.artist=artist;
+        this.length=length;
+    }
 
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+
+    static shortestAlbum(albums){
+        let shortest = albums[0].length;
+        let idx = -1
+        albums.forEach((album,i) => {
+            if(album.length<shortest){
+                shortest=album.length
+                idx=i
+            }
+        });
+        return albums[idx]
+    }
+}
 // don't change below
 module.exports = Music;
--- main.js
+++ main.js
@@ -2,3 +2,4 @@
 const Media = require("./classes/Media");
 const Book = require("./classes/Book")
 const Movie =  require("./classes/Movie")
+const Music = require("./classes/Music")
