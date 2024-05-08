5a04d4c4451df87ea5445a5f7158455c85c7b4f6
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 14:36:10 +0100

    movies class

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,29 @@
 // import the Media class:
-
+const Media = require("./Media")
 // create your Movie class:
+class Movie extends Media {
+    constructor(title,year,genre,director,duration,rating){
+        super(title,year,genre)
+        this.director=director;
+        this.duration=duration;
+        this.rating=rating;
+    }
 
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+
+    static longestMovie(movies){
+        let longest = 0;
+        let idx = 0;
+        movies.forEach((movie,i) => {
+            if (movie.duration>longest){
+                longest=movie.duration
+                idx=i
+            }
+        });
+        return movies[idx]
+    }
+}
 // don't change below
 module.exports = Movie;
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // Import classes here to console.log and debug
 const Media = require("./classes/Media");
 const Book = require("./classes/Book")
+const Movie =  require("./classes/Movie")
